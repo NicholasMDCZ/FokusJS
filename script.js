ce17e4e420cbcbd52d18de7d1df3efc1af828287
Nicholas Castro <contabrpt2@hotmail.com>
2024-06-19 16:07:42 -0300

    capturados elementos html

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt= document.querySelector('.app__card-button--longo')
+const longoBt = document.querySelector('.app__card-button--longo')
+const startPause = document.querySelector('.start-pause')
+const imagem = document.querySelector('.app__image')
+const appTitle = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
