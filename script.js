c7813be9f73cc5c9c97e4c1f19b6180d178a06d7
Nicholas Castro <contabrpt2@hotmail.com>
2024-06-25 10:58:48 -0300

    capturado elemento span

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
+const iniciarOuPausarBt = document.querySelector('#start-pouse span')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
 const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
