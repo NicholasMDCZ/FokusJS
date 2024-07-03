314be71b0bb68da2a132224796a6cdb78bb96583
Nicholas Castro <contabrpt2@hotmail.com>
2024-07-03 16:14:58 -0300

    elemento capturado

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,7 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
+const ulTarefas = document.querySelector('app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -43,4 +44,8 @@ formAdicionarTarefa.addEventListener('submit', (evento) =>{
     }
     tarefas.push(tarefa)
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
+})
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
 })
