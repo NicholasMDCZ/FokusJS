f72008abbdf6043c33a6f840b86008dbfb378caf
Nicholas Castro <contabrpt2@hotmail.com>
2024-07-08 15:09:28 -0300

    A lista agora salva os itens no local Storage

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,7 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
-const ulTarefas = document.querySelector('app__section-task-list')
+const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -11,10 +11,9 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-        </svg>
     `
 
     const paragrafo = document.createElement('p')
@@ -31,6 +30,8 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    return li
 }
 
 btnAdicionarTarefa.addEventListener('click', ()=> {
