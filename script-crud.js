9e049dc57dabb82890542ddb7a1d4a1fe6dec630
Nicholas Castro <contabrpt2@hotmail.com>
2024-07-09 10:47:24 -0300

    agr ficou bonitinho kkk

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,10 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
     li.classList.add('app__section-task-list-item')
@@ -51,7 +55,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) =>{
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas
     textArea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
